1c2caed98fe2a5ac533380ae0151e12673fa38b1
--global <--global>
2023-12-02 18:22:36 -0800

    more updates on joystick.h and .c updated naming convention and added a decoding function within joystick.c to decode the direction.

---
+++ pong/hardware/joystick.h
@@ -0,0 +1,28 @@
+/**
+ * joystick.h
+ * This file contains a constructor, destructor (for Joystick) and methods to start and stop reading the joystick module
+*/
+
+#ifndef JOYSTICK_H
+#define JOYSTICK_H
+
+typedef struct Joystick {
+    char* xPin;
+    char* yPin;
+    int yReading;
+    int xReading;
+    int upDownDirection;        // [up:-1, none:0, down:1]
+    int leftRightDirection;     // [left:-1, none:0, right:1]
+    pthread_mutex_t mID;
+    pthread_t tID;
+} Joystick;
+
+Joystick* Joystick_new(char* xPin_file, char* yPin_file);
+
+void Joystick_destroy(Joystick* joystick);
+
+void startReading_JS(Joystick* joystick);
+
+void stopReading_JS(Joystick* joystick);
+
+#endif

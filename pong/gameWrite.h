27f4c2fcded446b0b162d228c20772b05b0a2b0b
Shakeeb <shakeebzacky@gmail.com>
2023-12-06 23:28:42 -0800

    Replaced i2c writing function rewrote writeData function in gameWrite.c

--- pong/gameWrite.h
+++ pong/gameWrite.h
@@ -5,8 +5,17 @@
 #define MATRIX2 0x71
 #define MATRIX3 0x72
 #define MATRIX4 0x73
-#define BITS_TO_WRITE
+#define BYTES_TO_WRITE 8
 
-void writeData(int i2cFileDesc, int boardSize, GameEncodings* encodings);
+typedef struct MatrixHardware {
+    unsigned char Matrix1;
+    unsigned char Matrix2;
+    unsigned char Matrix3;
+    unsigned char Matrix4;
+} MatrixHardware;
+
+void writeData(int i2cFileDesc, int boardSize, MatrixHardware* mat, GameEncodings* encodings);
+void initMatrix(Matrix m, unsigned char m1, unsigned char m2, unsigned char m3, unsigned char m4);
+void initializeI2C(void);
 
 #endif

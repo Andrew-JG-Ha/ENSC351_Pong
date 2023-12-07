7794ce910ac9d96fcefd8a7a9a745bd32b09d814
Shakeeb <shakeebzacky@gmail.com>
2023-12-06 23:40:22 -0800

    Updated gameWrite to have correct functionality

--- pong/gameWrite.c
+++ pong/gameWrite.c
@@ -15,5 +15,18 @@ void writeData(int i2cFileDesc, int boardSize, MatrixHardware* mat, GameEncoding
             writeToRegI2C(i2cFileDesc, mat->Matrix3, encodings->playerHalf2[i], BYTES_TO_WRITE);
         }
     }
+}
 
+MatrixHardware* generate_Matrix(unsigned char m1, unsigned char m2, unsigned char m3, unsigned char m4) {
+    MatrixHardware* newMatrix = calloc(sizeof(MatrixHardware));
+    newMatrix->Matrix1 = m1;
+    newMatrix->Matrix1 = m2;
+    newMatrix->Matrix1 = m3;
+    newMatrix->Matrix1 = m4;
+    return newMatrix;
+}
+
+void destroy_Matrix(MatrixHardware* m) {
+    free(m);
+    m = NULL;
 }
--- pong/gameWrite.h
+++ pong/gameWrite.h
@@ -1,10 +1,6 @@
 #ifndef GAMEWRITE_H
 #define GAMEWRITE_H
 
-#define MATRIX1 0x70
-#define MATRIX2 0x71
-#define MATRIX3 0x72
-#define MATRIX4 0x73
 #define BYTES_TO_WRITE 8
 
 typedef struct MatrixHardware {
@@ -15,7 +11,7 @@ typedef struct MatrixHardware {
 } MatrixHardware;
 
 void writeData(int i2cFileDesc, int boardSize, MatrixHardware* mat, GameEncodings* encodings);
-void initMatrix(Matrix m, unsigned char m1, unsigned char m2, unsigned char m3, unsigned char m4);
-void initializeI2C(void);
+MatrixHardware* generate_Matrix(unsigned char m1, unsigned char m2, unsigned char m3, unsigned char m4);
+void destroy_Matrix(MatrixHardware* m);
 
 #endif
